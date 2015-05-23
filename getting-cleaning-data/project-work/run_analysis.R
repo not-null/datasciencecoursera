a593dd5944aabcba00cf91d40bb8d2d4d0d5a50c
not-null <himanshu.rawat@gmail.com>
2015-05-23 19:03:23 +1000

    refactored the script - major changes

--- getting-cleaning-data/project-work/run_analysis.R
+++ getting-cleaning-data/project-work/run_analysis.R
@@ -8,42 +8,19 @@ setwd("/Users/himanshurawat/coursera/datasciencecoursera/getting-cleaning-data/p
 # Get hold of neccessary packages and load 
 library(dplyr)
 
-# Need to format below information
-# Data that has been provided, in a simpler terms can be linked as follow
-#   1. features.txt [561 X 2]. Every row (2nd col) is a observation label.
-#       for e.g tBodyAcc-mean()-X can be interpreted as "Time Body Accelerometer - Mean taken - along X axis"
-#
-#   2. X_test.txt [2497 X 561]. Every column is measurement value
-#
-#   3. Both (1) and (2) are linked. Every row (2nd col) in features.txt corresponds to column value in X_test.txt.
-#       
-#      Note: Same interpertation is applicable to X_train.txt as well.
-
-# Read the common data
-activity.labels <- read.table("./UCI HAR Dataset/activity_labels.txt", nrows=6,header = F, stringsAsFactors = FALSE)
-features <- read.table("./UCI HAR Dataset/features.txt", nrows=561,header = F, stringsAsFactors = FALSE)
-
-# Read the test data
-test.observations <- read.table("./UCI HAR Dataset/test/X_test.txt",nrows = 2947, header = F, stringsAsFactors = FALSE)
-test.activity <- read.table("./UCI HAR Dataset/test/y_test.txt", nrows = 2947,header = F, stringsAsFactors = FALSE)
-test.subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt",nrows = 2947, header = F, stringsAsFactors = FALSE)
-
-# Read the training data
-train.observations <- read.table("./UCI HAR Dataset/train/X_train.txt", nrows = 7352,header = F, stringsAsFactors = FALSE)
-train.activity <- read.table("./UCI HAR Dataset/train/y_train.txt", nrows = 7352,header = F, stringsAsFactors = FALSE)
-train.subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", nrows = 7352,header = F, stringsAsFactors = FALSE)
+# Read all the data
+files <- file.path("./UCI HAR Dataset", c("activity_labels.txt", "features.txt", "test/X_test.txt", "test/y_test.txt", "test/subject_test.txt", "train/X_train.txt", "train/y_train.txt", "train/subject_train.txt"))
+data <- lapply(files, read.table, header = F, stringsAsFactors = FALSE)
+names(data) <- gsub(".*/(.*)\\..*", "\\1", files)
 
 # Merge test and train data
-merged.observations <- bind_rows(test.observations,train.observations)
-merged.activity <- bind_rows(test.activity, train.activity)
-merged.subjects <- bind_rows(test.subjects, train.subjects)
+merged.observations <- bind_rows(data$X_test,data$X_train)
+merged.activity <- bind_rows(data$y_test, data$y_train)
+merged.subjects <- bind_rows(data$subject_test, data$subject_train)
 
-# Give col names to merged observation. It links features.txt
-colnames(merged.observations) <- features[,2]
-colnames(merged.activity) <- "Activity"
-colnames(merged.subjects) <- "Subjects"
-
+# Merge and give meaningful column names
 final.merge <- bind_cols(merged.subjects,merged.activity, merged.observations)
+colnames(final.merge) <- c("Subjects" , "Activity" , data$features[,2])
 
 # search for columns names having mean and std
 columns.filter <- grep("*-mean()|*-std()", names(final.merge) , ignore.case = TRUE)
@@ -53,13 +30,7 @@ columns.filter <- c(1,2,columns.filter)
 final.merge.filtered <- final.merge[,columns.filter]
 
 # Change the activity names to descriptive information
-final.merge.filtered$Activity <- as.character(final.merge.filtered$Activity)
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 1)] <- activity.labels[[2]][1]
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 2)] <- activity.labels[[2]][2]
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 3)] <- activity.labels[[2]][3]
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 4)] <- activity.labels[[2]][4]
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 5)] <- activity.labels[[2]][5]
-final.merge.filtered$Activity[which(final.merge.filtered$Activity == 6)] <- activity.labels[[2]][6]
+final.merge.filtered$Activity <- factor(final.merge.filtered$Activity, levels=c(1,2,3,4,5,6), labels=data$activity_labels$V2)
 
 #Give expanded and meaningful names
 names(final.merge.filtered)<-gsub("Acc", "Accelerometer", names(final.merge.filtered))
